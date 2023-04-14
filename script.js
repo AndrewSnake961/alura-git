9bdfa5f7a8ac1f291f0f4f5b3f869a88054a4168
Andres Builes <felipe.hbuiles@gmail.com>
2023-04-13 21:26:45 -0500

    Siendo creativos con la bandera

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ document.write("<h1>Hola</h1>")
  let brushPaint = screen.getContext("2d");
 
 
-// brushPaint.fillStyle ="lightgray" //Property
-//  brushPaint.fillRect(0,0,600,400); // Function
+brushPaint.fillStyle ="lightgray" //Property
+ brushPaint.fillRect(0,0,600,400); // Function
 
 brushPaint.fillStyle ="green" //Property
  brushPaint.fillRect(0,0,200,400); // Function
 
 brushPaint.fillStyle ="red" //Property
- brushPaint.fillRect(400,0,200,400); // Function
+
+brushPaint.fillRect(400,0,200,400); // Function
+
+brushPaint.fillStyle ="yellow" //Property
+brushPaint.beginPath();
+brushPaint.moveTo(300,200);
+brushPaint.lineTo(200,400);
+brushPaint.lineTo(400,400);
+brushPaint.fill();
+
+brushPaint.fillStyle ="blue" //Property
+brushPaint.beginPath();
+brushPaint.arc(300,200,50,0,2*3.14);
+brushPaint.fill();
