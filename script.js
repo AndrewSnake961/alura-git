48141799e09cac2f1dbb3bee2b9a516fb2133bbb
Andres Builes <felipe.hbuiles@gmail.com>
2023-04-13 21:12:24 -0500

    Diseñando una bandera con un pincel

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.write("<h1>Hola</h1>")
  let screen = document.querySelector("canvas");
- let brushPaint = screen.getContext("2d")
+ let brushPaint = screen.getContext("2d");
+
+
+// brushPaint.fillStyle ="lightgray" //Property
+//  brushPaint.fillRect(0,0,600,400); // Function
+
+brushPaint.fillStyle ="green" //Property
+ brushPaint.fillRect(0,0,200,400); // Function
+
+brushPaint.fillStyle ="red" //Property
+ brushPaint.fillRect(400,0,200,400); // Function
